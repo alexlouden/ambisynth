6226afb27e03b5a71d6478abe5c9563b277a9a0d
Alex Louden <alex@louden.com>
2017-03-12 21:37:58 +0800

    React togglebuttons

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,83 +6,131 @@ import Tone from 'Tone'
 
 class Track {
 
-    constructor(filename, name) {
-        this.filename = './assets/' + filename + '.mp3'
-        this.name = name
-        this.player = new Tone.Player({
-            "url" : this.filename,
-            "loop" : true,
-            "autostart": false
-        })
+  constructor(filename, name) {
+    this.filename = './assets/' + filename + '.mp3'
+    this.name = name
+    this.player = new Tone.Player({
+      url: this.filename,
+      loop: true,
+      autostart: false
+    })
+  }
+
+  onLoad() {
+    this.player.toMaster()
+    this.player.start()
+  }
+
+  get mute() {
+    return this.player.mute
+  }
+  set mute(val) {
+    this.player.mute = val
+  }
+
+  get volume() {
+    let db = this.player.volume.value
+    return Math.exp(db / 20) * 100
+  }
+  set volume(perc) {
+
+    if (perc == 0) {
+      this.mute = true
+      return
     }
 
-    onLoad() {
-        this.player.toMaster()
-        this.player.start()
-    }
-
-    get mute() {
-        return this.player.mute
-    }
-    set mute(val) {
-        this.player.mute = val
-    }
-
-    get volume() {
-        let db = this.player.volume.value
-        return Math.exp(db / 20) * 100
-    }
-    set volume(perc) {
-
-        if (perc == 0) {
-            this.mute = true
-            return
-        }
-
-        // Set volume as percentage 0-100
-        let db = 20 * Math.log(perc / 100)
-        this.player.volume.value = db
-        this.mute = false
-    }
+    // Set volume as percentage 0-100
+    let db = 20 * Math.log(perc / 100)
+    this.player.volume.value = db
+    this.mute = false
+  }
 }
 
 const tracks = [
-    new Track('track1/1-Bass', 'Bass'),
-    new Track('track1/2-Emotion', 'Emotion'),
-    new Track('track1/3-Glass', 'Glass'),
-    new Track('track1/4-Hats', 'Hats'),
-    new Track('track1/5-Kicks', 'Kick'),
-    new Track('track1/6-Beads', 'Beads')
+  new Track('track1/1-Bass', 'Bass'),
+  new Track('track1/2-Emotion', 'Emotion'),
+  new Track('track1/3-Glass', 'Glass'),
+  new Track('track1/4-Hats', 'Hats'),
+  new Track('track1/5-Kicks', 'Kick'),
+  new Track('track1/6-Beads', 'Beads')
 ]
 window.tracks = tracks
+
 
 Tone.Buffer.on('load', e => {
     tracks.map(track => track.onLoad())
     $('.slider input').trigger('input')
 })
 
-class Slider extends React.Component {
+
+class VolumeSlider extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {value: 50}
+    this.state = {volume: 50}
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value})
-    this.props.track.volume = this.state.value
+    this.setState({volume: event.target.value})
+    this.props.track.volume = this.state.volume
+  }
+
+  render() {
+    return (
+      <div className="range">
+        <input type="range" min="0" value={this.state.volume} max="100" step="1" onChange={this.handleChange}/>
+      </div>
+    )
+  }
+}
+
+class ToggleButton extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {active: false}
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    this.setState({active: !this.state.active})
+    this.props.onToggle(!this.state.active)
+  }
+
+  render() {
+    let activeClass = this.state.active ? 'active' : ''
+    let className = this.props.name + ' ' + activeClass
+    return (
+      <button className={className} onClick={this.handleClick}>{this.props.name}</button>
+    )
+  }
+}
+
+
+class Slider extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.onToggleMute = this.onToggleMute.bind(this)
+    this.onToggleSolo = this.onToggleSolo.bind(this)
+  }
+
+  onToggleMute(active) {
+    this.props.track.mute = active
+  }
+
+  onToggleSolo(active) {
+    // TODO solo
   }
 
   render() {
     return (
       <div className="slider">
         <h5>{this.props.track.name}</h5>
-        <button className="mute">mute</button>
-        <button className="solo">solo</button>
-        <div className="range">
-          <input type="range" min="0" value={this.state.value} max="100" step="1" onChange={this.handleChange}/>
-        </div>
+        <ToggleButton name="mute" key={this.props.id} onToggle={this.onToggleMute}/>
+        <ToggleButton name="solo" key={this.props.id} onToggle={this.onToggleSolo}/>
+        <VolumeSlider track={this.props.track} key={this.props.id}/>
       </div>
     )
   }
