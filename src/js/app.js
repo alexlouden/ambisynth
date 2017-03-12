b1faa7330681d69d726ea0e00c488d4c8be15993
Alex Louden <alex@louden.com>
2017-03-12 12:20:56 +0800

    Single track class

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,42 @@ import $ from 'jQuery'
 import Tone from 'Tone'
 
 class Track {
+
     constructor(filename, label) {
         this.filename = './assets/' + filename + '.mp3'
         this.label = label
+        this.player = new Tone.Player({
+            "url" : this.filename,
+            "loop" : true,
+            "autostart": false
+        })
+    }
+
+    onLoad() {
+        this.player.toMaster()
+        this.player.start()
+    }
+
+    get mute() {
+        return this.player.mute
+    }
+    set mute(val) {
+        this.player.mute = val
+    }
+
+    get volume() {
+        let db = this.player.volume.value
+        return Math.exp(db / 20) * 100
+    }
+    set volume(perc) {
+        // Set volume as percentage 0-100
+        let db = 20 * Math.log(perc / 100)
+        this.player.volume.value = db
+        this.mute = false
     }
 }
 
-let tracks = [
+const tracks = [
     new Track('track1/1-Bass', 'Bass'),
     new Track('track1/2-Emotion', 'Emotion'),
     new Track('track1/3-Glass', 'Glass'),
@@ -17,6 +46,7 @@ let tracks = [
     new Track('track1/5-Kicks', 'Kick'),
     new Track('track1/6-Beads', 'Beads')
 ]
+window.tracks = tracks
 
 // <div>
 //     <h5>Bass</h5>
@@ -25,27 +55,18 @@ let tracks = [
 //     <button>solo</button>
 // </div>
 
-let players = _.map(tracks, track =>
-    new Tone.Player({
-        "url" : track.filename,
-        "loop" : true,
-        "autostart": false
-    })
-)
 
 Tone.Buffer.on('load', e =>
-    _.each(players, player => {
-        player.toMaster()
-        player.start()
-    })
+    tracks.map(track => track.onLoad())
 )
 
-$('#tracks .mute').on('click', e => {
+$('#mixer .mute').on('click', e => {
     e.preventDefault()
-    let id = $(this).parent().index()
-    let player = players[id]
+    let id = $(this).closest('.slider').index()
+    let track = tracks[id]
     let input = $(this).siblings('input')
-    if (player.mute) {
+    debugger
+    if (track.mute) {
         input.val(50)
     } else {
         input.val(0)
@@ -54,20 +75,17 @@ $('#tracks .mute').on('click', e => {
 })
 
 
-let onChangeVolume = function(e){
+let onChangeVolume = function(e) {
 
-    let id = $(this).parent().index()
-    let player = players[id]
+    let id = $(this).closest('.slider').index()
+    let track = tracks[id]
 
-    if ($(this).val() == 0){
-        player.mute = true
+    if ($(this).val() == 0) {
+        track.mute = true
     } else {
-        player.mute = false
-        let perc = $(this).val() / 100
-        let vol = 20 * Math.log(perc)
-        player.volume.value = vol
+        track.volume = $(this).val()
     }
 }
 
-$('#tracks input').on('input', _.throttle(onChangeVolume, 30))
-$('#tracks input').trigger('input')
+$('#mixer input').on('input', _.throttle(onChangeVolume, 30))
+$('#mixer input').trigger('input')
