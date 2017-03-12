62d2838b61d6d4eabd22b5968bc29edb8d649241
Alex Louden <alex@louden.com>
2017-03-12 11:36:03 +0800

    Convert to es6

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ let tracks = [
     new Track('track1/4-Hats', 'Hats'),
     new Track('track1/5-Kicks', 'Kick'),
     new Track('track1/6-Beads', 'Beads')
-];
+]
 
 // <div>
 //     <h5>Bass</h5>
@@ -25,47 +25,49 @@ let tracks = [
 //     <button>solo</button>
 // </div>
 
-let players = _.map(tracks, function(track){
-    return new Tone.Player({
+let players = _.map(tracks, track =>
+    new Tone.Player({
         "url" : track.filename,
         "loop" : true,
         "autostart": false
-    });
-})
+    })
+)
 
-Tone.Buffer.on('load', function(e){
-    console.log('loaded');
-    _.each(players, function(player){
-        player.toMaster();
-        player.start();
-    });
-})
+Tone.Buffer.on('load', e =>
+    _.each(players, player => {
+        player.toMaster()
+        player.start()
+    })
+)
 
-$('#tracks button').on('click', function(e){
-    e.preventDefault();
-    let id = $(this).parent().index();
-    let player = players[id];
-    let input = $(this).siblings('input');
+$('#tracks .mute').on('click', e => {
+    e.preventDefault()
+    let id = $(this).parent().index()
+    let player = players[id]
+    let input = $(this).siblings('input')
     if (player.mute) {
-        input.val(50);
+        input.val(50)
     } else {
-        input.val(0);
+        input.val(0)
     }
-    input.trigger('input');
+    input.trigger('input')
 })
 
-$('#tracks input').on('input', function(e){
-    let id = $(this).parent().index();
-    let player = players[id];
+
+let onChangeVolume = function(e){
+
+    let id = $(this).parent().index()
+    let player = players[id]
 
     if ($(this).val() == 0){
-        player.mute = true;
+        player.mute = true
     } else {
-        player.mute = false;
-        let perc = $(this).val() / 100;
-        let vol = 20 * Math.log(perc);
-        player.volume.value = vol;
+        player.mute = false
+        let perc = $(this).val() / 100
+        let vol = 20 * Math.log(perc)
+        player.volume.value = vol
     }
-})
+}
 
-$('#tracks input').trigger('input');
+$('#tracks input').on('input', _.throttle(onChangeVolume, 30))
+$('#tracks input').trigger('input')
